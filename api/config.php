abf0a26e81b5c67b698895ad73774d1969250b9e
Dubucute <68496575+Dubucute@users.noreply.github.com>
2026-06-03 17:44:00 +0800

    Delete api directory

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// DubuHub Configuration
-// This file is server-side only - not accessible directly
-
-define('ADMIN_PASSWORD', '@Jesa1234');
-define('SESSION_KEY', 'dubuhub_admin_session');
